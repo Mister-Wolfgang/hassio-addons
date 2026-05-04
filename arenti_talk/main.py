8d36ce20e7641228d98eff11a495b509102f407a
Mister-Wolfgang <mister.wolfgang.69@gmail.com>
2026-05-04 15:49:03 +0000

    fix: ingress path injection via X-Ingress-Path header

--- arenti_talk/main.py
+++ arenti_talk/main.py
@@ -7,9 +7,9 @@ import shutil
 import tempfile
 from contextlib import asynccontextmanager
 
-from fastapi import FastAPI, HTTPException, UploadFile, File, BackgroundTasks
+from fastapi import FastAPI, HTTPException, UploadFile, File, BackgroundTasks, Request
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import FileResponse
+from fastapi.responses import FileResponse, HTMLResponse
 from pydantic import BaseModel
 
 from auth import ArentiSession
@@ -134,8 +134,12 @@ async def lifespan(app: FastAPI):
 app = FastAPI(title="2way Audio Arenti", lifespan=lifespan)
 
 @app.get("/")
-async def index():
-    return FileResponse("/app/static/index.html")
+async def index(request: Request):
+    ingress_path = request.headers.get("X-Ingress-Path", "").rstrip("/")
+    with open("/app/static/index.html") as f:
+        html = f.read()
+    html = html.replace("__INGRESS_PATH__", ingress_path)
+    return HTMLResponse(html)
 
 app.mount("/static", StaticFiles(directory="/app/static"), name="static")
 
